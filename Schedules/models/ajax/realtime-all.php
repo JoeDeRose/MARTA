07ca7b8ca1d675312e507aa6030023fbbcf7ed7e
Joe DeRose <joe@joederose.us>
2013-04-09 19:48:48 -0400

    Remove some realtime deleted (test) files

--- Schedules/models/ajax/realtime-all.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-type: application/json');
-
-$RealTimeJSON = file_get_contents( "http://developer.itsmarta.com/BRDRestService/BRDRestService.svc/GetAllBus" );
-
-echo( $RealTimeJSON );
-?>
--- Schedules/models/ajax/realtime-old.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if ( isset( $_GET["route"] ) ) {
-		$URL = "http://developer.itsmarta.com/BRDRestService/BRDRestService.svc/GetBusByRoute/" . $_GET["route"];
-	} else {
-		$URL = "http://developer.itsmarta.com/BRDRestService/BRDRestService.svc/GetAllBus";
-	}
-	$content = file_get_contents( $URL );
-	echo $content;
-?>
