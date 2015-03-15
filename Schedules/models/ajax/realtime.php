de037461bf82755388856d88594414af8c166b31
Joe DeRose <derose.joe@gmail.com>
2015-03-15 18:13:23 -0400

    fix: handle TRIPID=0 situation

--- Schedules/models/ajax/realtime.php
+++ Schedules/models/ajax/realtime.php
@@ -22,10 +22,10 @@ $RealTimeResult = json_decode( $RealTimeJSON );
 
 foreach( $RealTimeResult as $arraykey => $arrayvalue ):
 
-	if ( $RealTimeResult[$arraykey] -> TRIPID != "" ) {
+	if ( $arrayvalue -> TRIPID != "" && $arrayvalue -> TRIPID != "0" ) {
 
-		$RealTimeResult[$arraykey] -> COORDINDATES = $RealTimeResult[$arraykey] -> LATITUDE . ',' . $RealTimeResult[$arraykey] -> LONGITUDE;
-		$ThisTripID = $RealTimeResult[$arraykey] -> TRIPID;
+		$arrayvalue -> COORDINDATES = $arrayvalue -> LATITUDE . ',' . $arrayvalue -> LONGITUDE;
+		$ThisTripID = $arrayvalue -> TRIPID;
 
 // HEREDOC ------------------------------------------------------------------------
 	$qClosestStop = <<<Q_CLOSEST_STOP
@@ -43,7 +43,7 @@ FROM
       S.stop_name,
       S.stop_lat,
       S.stop_lon,
-      ABS( S.stop_lat - {$RealTimeResult[$arraykey] -> LATITUDE} ) + ABS ( S.stop_lon - {$RealTimeResult[$arraykey] -> LONGITUDE} ) AS dist
+      ABS( S.stop_lat - {$arrayvalue -> LATITUDE} ) + ABS ( S.stop_lon - {$arrayvalue -> LONGITUDE} ) AS dist
     FROM routes R
       INNER JOIN trips T
         ON R.route_id = T.route_id
@@ -62,115 +62,20 @@ Q_CLOSEST_STOP;
 
 		$ClosestStop = $mysqli -> query( $qClosestStop );
 		$row = $ClosestStop -> fetch_assoc();	// One-row query, so I don't need a while(){} construct.
-		$RealTimeResult[$arraykey] -> HEADSIGN = $row["trip_headsign"];
-		$RealTimeResult[$arraykey] -> NEAREST_STOP_CODE = $row["stop_code"];
-		$RealTimeResult[$arraykey] -> NEAREST_STOP_NAME = $row["stop_name"];
-		$RealTimeResult[$arraykey] -> NEAREST_STOP_SEQUENCE = $row["stop_sequence"];
-		$RealTimeResult[$arraykey] -> NEAREST_STOP_LATITUDE = $row["stop_lat"];
-		$RealTimeResult[$arraykey] -> NEAREST_STOP_LONGITITUDE = $row["stop_lon"];
-		$RealTimeResult[$arraykey] -> NEAREST_STOP_DIRECTIONID = $row["direction_id"];
+		$arrayvalue -> HEADSIGN = $row["trip_headsign"];
+		$arrayvalue -> NEAREST_STOP_CODE = $row["stop_code"];
+		$arrayvalue -> NEAREST_STOP_NAME = $row["stop_name"];
+		$arrayvalue -> NEAREST_STOP_SEQUENCE = $row["stop_sequence"];
+		$arrayvalue -> NEAREST_STOP_LATITUDE = $row["stop_lat"];
+		$arrayvalue -> NEAREST_STOP_LONGITITUDE = $row["stop_lon"];
+		$arrayvalue -> NEAREST_STOP_DIRECTIONID = $row["direction_id"];
 	
-		$AdherenceValue = intval( $RealTimeResult[$arraykey] -> ADHERENCE );
-		$MessageTime = DateTime::createFromFormat( 'n/j/Y g:i:s a', $RealTimeResult[$arraykey] -> MSGTIME );
-		$RealTimeResult[$arraykey] -> NEAREST_STOP_SCHEDULED_TIME = $row["arrival_time"];
+		$AdherenceValue = intval( $arrayvalue -> ADHERENCE );
+		$MessageTime = DateTime::createFromFormat( 'n/j/Y g:i:s a', $arrayvalue -> MSGTIME );
+		$arrayvalue -> NEAREST_STOP_SCHEDULED_TIME = $row["arrival_time"];
 		$NowDateTime = new DateTime();
 		$NowDateTimeValue = $NowDateTime -> format( "G:i:s" );
-		$RealTimeResult[$arraykey] -> NOW = $NowDateTimeValue;
-//		$MessageAge = $MessageTime -> diff( $NowDateTime );
-//		$MessageAgeText = $MessageAge -> i;
-//		$RealTimeResult[$arraykey] -> MESSAGE_AGE = $MessageAgeText;
-//		$MessageAgeDiff = $MessageAgeText * 100;
-
-// The Following prediction content was based on the (incorrect) assumption that the MSGTIME value
-// in the MARTA JSON response was the time that the bus location was last updated.
-/*
-// HEREDOC ------------------------------------------------------------------------
-	$qPredictionStop = <<<Q_PREDICTION_STOP
-
-SELECT Q1.*
-FROM
-  (
-    SELECT
-      ST.*,
-      S.stop_code,
-      S.stop_name,
-	  S.stop_lon,
-	  S.stop_lat,
-      abs( timediff( ST.arrival_time, '{$row["arrival_time"]}' ) - {$MessageAgeDiff} ) AS DIFF
-    FROM stop_times ST
-      INNER JOIN stops S
-        ON ST.stop_id = S.stop_id
-    WHERE
-      ST.trip_id = {$RealTimeResult[$arraykey] -> TRIPID}
-      AND ST.stop_sequence >= {$row["stop_sequence"]}
-    ORDER BY
-      DIFF,
-      ST.stop_sequence
-  ) Q1
-LIMIT 1;
-
-Q_PREDICTION_STOP;
-// --------------------------------------------------------------------------------
-		$PredictionStop = $mysqli -> query( $qPredictionStop );
-		$row1 = $PredictionStop -> fetch_assoc();	// One-row query, so I don't need a while(){} construct.
-		$RealTimeResult[$arraykey] -> PREDICTION_LATITUDE = $row1["stop_lat"];
-		$RealTimeResult[$arraykey] -> PREDICTION_LONGITUDE = $row1["stop_lon"];
-		$RealTimeResult[$arraykey] -> PREDICTION_CODE = $row1["stop_code"];
-		$RealTimeResult[$arraykey] -> PREDICTION_NAME = $row1["stop_name"];
-*/
-
-// The Following prediction content is based on the (revised) understanding that the MSGTIME value
-// in the MARTA JSON response is the time that the bus last reached a timepoint stop.
-/*
-// HEREDOC ------------------------------------------------------------------------
-	$qPredictionStop = <<<Q_PREDICTION_STOP
-
-SELECT Q1.*
-FROM
-  (
-    SELECT
-      ST.*,
-      S.stop_code,
-      S.stop_name,
-  	  S.stop_lon,
-  	  S.stop_lat,
-      abs
-        (
-          timediff
-            (
-              ST.arrival_time,
-                (
-                  SELECT ST1.arrival_time
-                  FROM stop_times ST1
-                    INNER JOIN stops S1
-                      ON ST1.stop_id = S1.stop_id
-                  WHERE
-                    ST1.trip_id = {$RealTimeResult[$arraykey] -> TRIPID}
-                    AND S1.stop_code = {$RealTimeResult[$arraykey] -> STOPID}
-                )
-            ) - {$MessageAgeDiff}
-        ) AS DIFF
-    FROM stop_times ST
-      INNER JOIN stops S
-        ON ST.stop_id = S.stop_id
-    WHERE
-      ST.trip_id = {$RealTimeResult[$arraykey] -> TRIPID}
-    ORDER BY
-      DIFF,
-      ST.stop_sequence
-  ) Q1
-LIMIT 1;
-
-Q_PREDICTION_STOP;
-// --------------------------------------------------------------------------------
-		$PredictionStop = $mysqli -> query( $qPredictionStop );
-		$row1 = $PredictionStop -> fetch_assoc();	// One-row query, so I don't need a while(){} construct.
-		$RealTimeResult[$arraykey] -> PREDICTION_LATITUDE = $row1["stop_lat"];
-		$RealTimeResult[$arraykey] -> PREDICTION_LONGITUDE = $row1["stop_lon"];
-		$RealTimeResult[$arraykey] -> PREDICTION_CODE = $row1["stop_code"];
-		$RealTimeResult[$arraykey] -> PREDICTION_NAME = $row1["stop_name"];
-		$RealTimeResult[$arraykey] -> PREDICTION_SEQUENCE = $row1["stop_sequence"];
-*/
+		$arrayvalue -> NOW = $NowDateTimeValue;
 
 // HEREDOC ------------------------------------------------------------------------
 	$qNextStop = <<<Q_NEXT_STOP
@@ -190,8 +95,8 @@ FROM
       INNER JOIN stops S
         ON ST.stop_id = S.stop_id
     WHERE
-      T.trip_id = {$RealTimeResult[$arraykey] -> TRIPID}
-      AND ST.stop_sequence >= {$RealTimeResult[$arraykey] -> NEAREST_STOP_SEQUENCE}
+      T.trip_id = {$arrayvalue -> TRIPID}
+      AND ST.stop_sequence >= {$arrayvalue -> NEAREST_STOP_SEQUENCE}
     ORDER BY ST.stop_sequence
     LIMIT 2
   ) Q1
@@ -202,22 +107,22 @@ Q_NEXT_STOP;
 // --------------------------------------------------------------------------------
 		$NextStop = $mysqli -> query( $qNextStop );
 		$row2 = $NextStop -> fetch_assoc();	// One-row query, so I don't need a while(){} construct.
-		$RealTimeResult[$arraykey] -> NEXT_STOP_SEQUENCE = $row2["stop_sequence"];
-		$RealTimeResult[$arraykey] -> NEXT_STOP_CODE = $row2["stop_code"];
-		$RealTimeResult[$arraykey] -> NEXT_STOP_NAME = $row2["stop_name"];
-		$RealTimeResult[$arraykey] -> NEXT_STOP_LATITUDE = $row2["stop_lat"];
-		$RealTimeResult[$arraykey] -> NEXT_STOP_LONGITUDE = $row2["stop_lon"];
-		if ( $RealTimeResult[$arraykey] -> NEXT_STOP_SEQUENCE == $RealTimeResult[$arraykey] -> NEAREST_STOP_SEQUENCE ) {
-			$RealTimeResult[$arraykey] -> BUS_BEARING_END_OF_LINE = "Y";
-			$RealTimeResult[$arraykey] -> BUS_BEARING_ANGLE = 0;
+		$arrayvalue -> NEXT_STOP_SEQUENCE = $row2["stop_sequence"];
+		$arrayvalue -> NEXT_STOP_CODE = $row2["stop_code"];
+		$arrayvalue -> NEXT_STOP_NAME = $row2["stop_name"];
+		$arrayvalue -> NEXT_STOP_LATITUDE = $row2["stop_lat"];
+		$arrayvalue -> NEXT_STOP_LONGITUDE = $row2["stop_lon"];
+		if ( $arrayvalue -> NEXT_STOP_SEQUENCE == $arrayvalue -> NEAREST_STOP_SEQUENCE ) {
+			$arrayvalue -> BUS_BEARING_END_OF_LINE = "Y";
+			$arrayvalue -> BUS_BEARING_ANGLE = 0;
 		} else {
-			$BearingLatitudeDifference = ( $RealTimeResult[$arraykey] -> NEAREST_STOP_LATITUDE - $row2["stop_lat"] ) * $haversineratio;
-			$BearingAdjacentAngle = $row2["stop_lat"] - $RealTimeResult[$arraykey] -> NEAREST_STOP_LATITUDE;
-			$BearingHypotenuse = sqrt( pow( $row2["stop_lat"] - $RealTimeResult[$arraykey] -> NEAREST_STOP_LATITUDE, 2 ) + pow( ( $row2["stop_lon"] - $RealTimeResult[$arraykey] -> NEAREST_STOP_LONGITITUDE ) * $haversineratio, 2 ) );
-			$BearingSign = ( $row2["stop_lon"] - $RealTimeResult[$arraykey] -> NEAREST_STOP_LONGITITUDE < 0 ? -1 : 1 );
+			$BearingLatitudeDifference = ( $arrayvalue -> NEAREST_STOP_LATITUDE - $row2["stop_lat"] ) * $haversineratio;
+			$BearingAdjacentAngle = $row2["stop_lat"] - $arrayvalue -> NEAREST_STOP_LATITUDE;
+			$BearingHypotenuse = sqrt( pow( $row2["stop_lat"] - $arrayvalue -> NEAREST_STOP_LATITUDE, 2 ) + pow( ( $row2["stop_lon"] - $arrayvalue -> NEAREST_STOP_LONGITITUDE ) * $haversineratio, 2 ) );
+			$BearingSign = ( $row2["stop_lon"] - $arrayvalue -> NEAREST_STOP_LONGITITUDE < 0 ? -1 : 1 );
 			$BearingAngle = rad2deg( acos( $BearingAdjacentAngle / $BearingHypotenuse ) ) * $BearingSign;
-			$RealTimeResult[$arraykey] -> BUS_BEARING_END_OF_LINE = "N";
-			$RealTimeResult[$arraykey] -> BUS_BEARING_ANGLE = $BearingAngle;
+			$arrayvalue -> BUS_BEARING_END_OF_LINE = "N";
+			$arrayvalue -> BUS_BEARING_ANGLE = $BearingAngle;
 		}
 
 // HEREDOC ------------------------------------------------------------------------
@@ -236,7 +141,7 @@ FROM trips T
   INNER JOIN stops S
     ON ST.stop_id = S.stop_id
 WHERE
-  T.trip_id = {$RealTimeResult[$arraykey] -> TRIPID}
+  T.trip_id = {$arrayvalue -> TRIPID}
   AND timediff( ST.arrival_time, '{$NowDateTimeValue}' ) >= 0
 
 Q_TIMEPREDICTIONSINSERT;
@@ -304,7 +209,7 @@ endif;
 $NumberOfVehicles = count( $RealTimeResult ) - $VehiclesWithMissingTripID;
 $ThisVehicle = 0;
 foreach( $RealTimeResult as $key => $value ):
-	if ( $value -> TRIPID != "" ) :
+	if ( $value -> TRIPID != "" && $value -> TRIPID != "0" ) :
 		$ThisVehicle++;
 		$Comma = ( $ThisVehicle == $NumberOfVehicles ) ? "" : ",";
 ?>
