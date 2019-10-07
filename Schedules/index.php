f829469bb03ab9ce04fa645fa1f7c455de63aa89
Joe DeRose <derose.joe@gmail.com>
2019-10-06 21:22:01 -0400

    chore: GTFS Update of 2019-09-25

--- Schedules/index.php
+++ Schedules/index.php
@@ -55,21 +55,6 @@ if ( isset( $_GET["fullscreen"] ) ) {
 	</div>
 </div>
 
-<!-- Google Analytics
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-37469097-1']);
-  _gaq.push(['_setDomainName', 'joederose.us']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
--->
-
 <!-- Google Maps -->
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyCF2hkVUVY70uZdaxiQfYwlh7f3P4CNVKw"></script>
 
