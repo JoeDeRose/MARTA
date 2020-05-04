76f25520400a749444c08d35bf9b618e858fe76f
Joe DeRose <derose.joe@gmail.com>
2020-05-03 21:35:14 -0400

    chore: GTFS Update of 2020-04-27

--- Data/Diagnostics.sql
+++ Data/Diagnostics.sql
@@ -260,7 +260,7 @@ ORDER BY
 COMMIT;
 
 -- Find stop code for Five Points Station.
--- Most recent value is 908981; if changed:
+-- Most recent value is 797; if changed:
 --   (1) Update the most recent value in the line above;
 --   (2) Use the new value to populate line 186 of
 --       C:\Users\Joe DeRose\Documents\My Webs\Joe - Bluehost\MARTA\Schedules\models\route.php
